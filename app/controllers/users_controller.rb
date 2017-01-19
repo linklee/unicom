ca0da6da2cf17b2574e6d8ad8ecb4d82dad0005d
linklee <nevernight721@gmail.com>
2017-01-20 00:59:29 +0500

    jobs... steve jobs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_admin! 
 
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy] 
   # GET /users
   # GET /users.json
   def index
