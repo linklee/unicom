3beb4e13a498c4687303cc75cbf3cf6eea528b6e
linklee <nevernight721@gmail.com>
2017-01-28 21:39:42 +0500

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -80,7 +80,7 @@ class ApplicationController < ActionController::Base
 	  test_url = "http://xxx.requestcatcher.com/?clickid=#{click_id}&goal=1&status=#{status}"
 	  ok_test_url = URI.parse(URI.encode(test_url))
 
-	  url = "http://offers.atgmedia.affise.com/postback??clickid=#{click_id}&goal=1&status=#{status}" 
+	  url = "http://offers.atgmedia.affise.com/postback?clickid=#{click_id}&goal=1&status=#{status}" 
 	  encoded_url = URI.encode(url)
 	  ok_url = URI.parse(encoded_url)
 	  HTTParty.get ok_url
