8943e255aeb9526096592229cbfc57fcd60bb5d4
linklee <nevernight721@gmail.com>
2017-01-28 21:01:41 +0500

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
-
+  require 'httparty'
   include ApplicationHelper
 
   def index 
@@ -80,7 +80,7 @@ class ApplicationController < ActionController::Base
 	  encoded_url = URI.encode(url)
 	  ok_url = URI.parse(encoded_url)
 	  HTTParty.get ok_url
-	  render html: "ok"
+	  render html: "ok #{status}"
   end
 
 end
